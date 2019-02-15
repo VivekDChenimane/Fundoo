f8b4c44382950fa82c900eff99381d8ac61d9372
VivekDChenimane <46102265+VivekDChenimane@users.noreply.github.com>
2019-02-15 23:50:58 +0530

    login done
    ...

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
+import {  HttpService} from '../../service/http/http.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service : HttpService, private router : Router) { }
 
   ngOnInit() {
   }
+/**
+   * @description Hide and show password
+   */
+  hide = true; 
 
+  /**
+   * @description email validation 
+   */
+  email = new FormControl('', [Validators.required, Validators.email]); 
+
+  /**
+   * @description Password validation
+   */
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  
+  /**
+   * @description Gets mail error message
+   */
+  getErrorMessage() {
+    return this.email.hasError('required') ?
+      'You must enter a value' : this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
+  /**
+   * @description Gets password error message
+   */
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? "Can't be empty" : this.password.hasError('minlength') ? 'Wrong password' : '';
+  }
+  
+  /**
+   * @description Login after validating the data and checking the database whether the user exists or not
+   * @param email 
+   * @param password 
+   */
+  secureLogin(email, password) {
+    var user = {
+      "email": email,
+      "password": password
+    }
+    console.log(user);
+    
+    this.service.postRequest(user, 'login').subscribe((data: any)  =>  {
+      localStorage.setItem('token', data.token);
+      // localStorage.setItem('email', data.email);
+      //var a = (this.auth.isAuthenticated());
+      if (data != 'undefined') {
+        if (data.success) {
+           this.router.navigate(['home/notes']);
+        } else {
+          alert(data.msg)
+        }
+      }
+    });
+  }
+
+/**
+ * @description Navigates to registration page
+ */
+  register() { 
+    this.router.navigate(['registration']);
+  }
+  
+/**
+ * @description Navigates to forgot password page
+ */
+  forgot() {
+    this.router.navigate(['forgot']);
+  }
 }
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -14,27 +14,57 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
+    /**
+   * @description firstName validation 
+   */
   firstName = new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]*')]);
+    /**
+   * @description lastName validation 
+   */
   lastName = new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]*')]);
+    /**
+   * @description email validation 
+   */
   email = new FormControl('',[Validators.required,Validators.email]);
+    /**
+   * @description phone validation 
+   */
   phone = new FormControl('',[Validators.required,Validators.pattern('[0-9]*'),Validators.maxLength(12),Validators.minLength(10)]);
+    /**
+   * @description password validation 
+   */
   password = new FormControl('',[Validators.required,Validators.minLength(8)]);
+    /**
+   * @description repeatPassword validation 
+   */
   repeatPassword = new FormControl('',[Validators.required,Validators.minLength(8)]);
+   /**
+   * @description Gets firstName error message
+   */
   firstNameErrorMessage(){
     return this.firstName.hasError('required') ? 'Enter the name':
     this.firstName.hasError('pattern') ? 'Enter a valid name':
     '';
   }
+   /**
+   * @description Gets lastName error message
+   */
   lastNameErrorMessage(){
     return this.lastName.hasError('required') ? 'Enter the name':
     this.lastName.hasError('pattern') ? 'Enter a valid name':
     '';
   }
+   /**
+   * @description Gets mail error message
+   */
   emailErrorMessage(){
     return this.email.hasError('required') ? 'email is required':
     this.email.hasError('email') ? 'Not a valid email':
     '';
   }
+   /**
+   * @description Gets phone error message
+   */
   phoneErrorMessage(){
     return this.phone.hasError('required') ? 'Enter the phone number':
     this.phone.hasError('pattern') ? 'Enter numbers only':
@@ -42,16 +72,23 @@ export class RegisterComponent implements OnInit {
     this.phone.hasError('maxlength') ? 'Should be of maximum of 12':
     '';
   }
+   /**
+   * @description Gets password error message
+   */
   passwordErrorMessage(){
     return this.password.hasError('required') ? 'Enter the password':
     this.password.hasError('minlength') ? 'The password should of minimum of 6 digits':
     '';
   }
+   /**
+   * @description Gets repeatPassword error message
+   */
   repeatPasswordErrorMessage(){
     return this.repeatPassword.hasError('required') ? 'Enter the password':
     this.repeatPassword.hasError('minlength') ? 'The password should of minimum of 6 digits':
     '';
   }
+  
   getValues ( firstName,lastName,email,phone,password,repeatPassword){
     var user = {
       "firstName":firstName,
